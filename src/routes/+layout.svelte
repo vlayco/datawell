dce240235ac091b51fa6cbdbd960dfd3dcfdaf8f
Vladimir Vlayco <vladaj00@gmail.com>
2023-08-22 19:51:15 +0200

    theme toggler logic and tailwindcss styles added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.css';
+  import ThemeSwitch from '$lib/ThemeSwitch/ThemeSwitch.svelte';
 </script>
 
-<div class="container mx-auto w-auto min-h-screen bg-sky-600 text-white">
-	<slot />
+<div class="w-auto min-h-screen bg-sky-100 text-sky-950 dark:bg-sky-950 dark:text-sky-100">
+	<ThemeSwitch />
+  <slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-center h-screen w-auto">
+  <h1 class="text-4xl">Start of DataWell</h1>
+  <p class="text-2xl">Stay tunned in!</p>
+</div>
+
+
