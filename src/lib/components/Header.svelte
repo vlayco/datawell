ab533bf4e854dcb3c1eca4a81279ec3fc34ad055
Vladimir Vlayco <vladaj00@gmail.com>
2023-08-24 23:10:34 +0200

    some small fixes in header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,15 +3,15 @@
 
 </script>
 
-<header class="flex flex-col md:flex-row items-center justify-around md:justify-between px-8 text-center h-40 md:h-20 overflow-hidden">
+<header class="flex flex-col md:flex-row items-center justify-around md:justify-between space-y-1 px-8 text-center h-fit md:h-20 overflow-hidden w-full">
   <!-- Logo -->
-  <div class="md:mt-7 bg-logo-light-mode dark:bg-logo-dark-mode bg-no-repeat h-20 w-48 mx-auto md:mx-0 object-center"></div>
+  <div class="mt-4 md:mt-7 bg-logo-light-mode dark:bg-logo-dark-mode bg-no-repeat h-20 w-44 mx-auto md:mx-0"></div>
 
-  <div class="flex-col md:flex-row items-center justify-center space-x-4 md:space-x-10 md:absolute">
+  <div class="flex flex-col md:flex-row items-center justify-center space-x-4 md:space-x-10 space-y-2 md:space-y-0">
     <a href="#features" class="hover:text-accentCyan">Features</a>
     <a href="#testimonials" class="hover:text-accentCyan">Testimonials</a>
+    <ThemeSwitch />
   </div>
 
-  <ThemeSwitch />
 </header>
 
