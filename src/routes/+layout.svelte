f59bff9777b3c103bcd3c9fa9ffd1105c89c25e1
Vladimir Vlayco <vladaj00@gmail.com>
2023-08-24 23:44:05 +0200

    added hero section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script>
 	import '../app.css';
-  import ThemeSwitch from '$lib/ThemeSwitch/ThemeSwitch.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<div class="font-dmsans w-auto h-fit bg-sky-100 text-sky-950 dark:bg-darkBlue dark:text-sky-100">
-  <main class="container mx-auto flex flex-col h-screen">
+<div class="font-dmsans w-auto bg-sky-100 text-sky-950 dark:bg-darkBlue dark:text-sky-100">
+  <main class="container mx-auto flex flex-col min-h-screen">
     <Header />
     <slot />
     <Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
-<div class="flex flex-col items-center justify-center h-auto w-auto">
-  <h1 class="text-6xl">Start of DataWell</h1>
-  <p class="text-4xl">Stay tunned in!</p>
-  <p class="text-3xl">Some random text</p>
-</div>
+<!-- Hero section -->
+<section class="bg-curvy-light-mode dark:bg-curvy-dark-mode bg-no-repeat bg-contain bg-bottom">
+  <div class="container mx-auto px-6 text-center md:pt-20 pb-52 h-[900px]">
+    <img src="/images/illustration-intro.png" alt="" class="mx-auto">
+
+    <h1 class="mt-14 max-w-2xl mx-auto mb-10 text-3xl font-bold leading-normal md:text-4xl">Access your valuable files wherever you are, from a central, secure location!</h1>
+
+    <p class="max-w-sm mx-auto mb-10 text-sm md:max-w-xl md:text-lg">Datawell stores all your most important files in one secure location. Access them as you need, share and collaborate with friends, family, and co-workers.
+    </p>
+
+    <button class="p-3 rounded-full w-52 bg-accentCyan hover:scale-95">Get Started</button>
+  </div>
+</section>
 
 
