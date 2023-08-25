176b586f3a008a041311e96b6771679ce5fdeff8
Vladimir Vlayco <vladaj00@gmail.com>
2023-08-25 16:54:37 +0200

    features section added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <!-- Hero section -->
 <section class="bg-curvy-light-mode dark:bg-curvy-dark-mode bg-no-repeat bg-contain bg-bottom">
+
   <div class="container mx-auto px-6 text-center md:pt-20 pb-52 h-[900px]">
     <img src="/images/illustration-intro.png" alt="" class="mx-auto">
 
@@ -9,6 +10,102 @@
     </p>
 
     <button class="p-3 rounded-full w-52 bg-accentCyan hover:scale-95">Get Started</button>
+  </div>
+
+</section>
+
+<!-- Features section -->
+<section id="features" class="pt-12 bg-gray-50 dark:bg-darkBlue1">
+  
+  <!-- Features container -->
+  <div class="container mx-auto px-6 pb-32">
+
+  <!-- First Row -->
+  <div class="flex flex-col space-y-24 text-center md:flex-row md:space-y-0">
+
+    <!-- First Item -->
+    <div class="flex flex-col items-center space-y-2 md:w-1/2">
+      <div class="flex items-center justify-center h-24 mb-6">
+        <img src="/images/icon-access-anywhere.svg" alt="" />
+      </div>
+      <h3 class="text-xl font-bold">Access your files from anywhere</h3>
+      <p class="max-w-md">The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.</p>
+    </div>
+
+    <!-- Second Item -->
+    <div class="flex flex-col items-center space-y-2 md:w-1/2">
+      <div class="flex items-center justify-center h-24 mb-6">
+        <img src="/images/icon-security.svg" alt="" />
+      </div>
+      <h3 class="text-xl font-bold">Security that you can trust</h3>
+      <p class="max-w-md">Two-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files.</p>
+    </div>
+
+  </div>
+
+  <!-- Second Row -->
+  <div class="flex flex-col space-y-24 mt-28 text-center md:flex-row md:space-y-0">
+
+    <!-- Third Item -->
+    <div class="flex flex-col items-center space-y-2 md:w-1/2">
+      <div class="flex items-center justify-center h-24 mb-6">
+        <img src="/images/icon-collaboration.svg" alt="" />
+      </div>
+      <h3 class="text-xl font-bold">Lorem ipsum dolor sit amet consectetur.</h3>
+      <p class="max-w-md">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus dignissimos fugiat perspiciatis obcaecati cum exercitationem eos labore similique, ut ducimus.</p>
+    </div>
+
+    <!-- Fourth Item -->
+    <div class="flex flex-col items-center space-y-2 md:w-1/2">
+      <div class="flex items-center justify-center h-24 mb-6">
+        <img src="/images/icon-any-file.svg" alt="" />
+      </div>
+      <h3 class="text-xl font-bold">Tempore, expedita veritatis distinctio</h3>
+      <p class="max-w-md">Sed facilis natus doloremque facere reiciendis soluta aliquam? Exercitationem quas fugit dolor? Iste, repudiandae velit deleniti doloribus dicta eaque non quo eligendi harum aut ad.</p>
+      
+    </div>
+
+  </div>
+
+  </div>
+</section>
+
+<!-- Productive section -->
+<section id="productive" class="bg-white dark:bg-darkBlue">
+  
+  <!-- Productive container -->
+  <div class="container flex flex-col items-center mx-auto px-6 pt-24 pb-32 md:flex-row md:space-x-16">
+
+    <!-- Image -->
+    <div class="md:w-1/2">
+      <img src="/images/illustration-stay-productive.png" alt="" class="mb-10">
+    </div>
+
+    <!-- Content -->
+    <div class="flex flex-col items-start md:w-1/2">
+      <div class="flex flex-col space-y-5">
+        <h4 class="max-w-md text-xl font-bold md:text-4xl">
+          Stay productive wherever you are
+        </h4>
+        <p class="text-md md:text-large">
+          Never let location be an issue when accessing your files. Datawell has you covered for all of your file storage needs.
+        </p>
+
+        <p class="text-md md:text-large">
+          Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.
+        </p>
+      </div>
+
+      <!-- Link -->
+      <div class="block mt-4">
+        <a href="#" class="border-b border-x-accentCyan text-accentCyan">
+          See how Datawell works
+          <img src="/images/icon-arrow.svg" alt="" class="inline pb-2">
+        </a>
+      </div>
+
+    </div>
+
   </div>
 </section>
 
