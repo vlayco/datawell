6eb65e50ae857204c6c0034d7cb6b6bf59e3c7be
Vladimir Vlayco <vladaj00@gmail.com>
2023-08-25 23:24:43 +0200

    footer added

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,1 +1,61 @@
-<footer>Footer component</footer>
+<footer class="bg-darkBlue2 text-accentCyan">
+  <div class="container mx-auto pt-12 px-5 pb-10">
+
+    <!-- Flex Container For Entire Time -->
+    <div class="flex flex-col justify-between space-y-24 md:flex-row md:space-y-0">
+
+      <!-- Email & Phone -->
+      <div class="mt-10 space-y-6">
+        <div class="flex items-center space-x-3 md:-mt-10">
+          <div class="w-6">
+            <img src="/images/icon-phone.svg" alt="" class="scale-10" />
+          </div>
+          <div>+3816789101234</div>
+        </div>
+
+        <div class="flex items-center space-x-3">
+          <div class="w-6">
+            <img src="/images/icon-email.svg" alt="" class="scale-10" />
+          </div>
+          <div>example@datawell.com</div>
+        </div>
+      </div>
+
+      <!-- Menus -->
+      <div class="flex flex-col space-y-10 text-xl md:text-base md:space-x-20 md:space-y-0 md:flex-row">
+        <div class="flex flex-col space-y-3">
+          <a href="#">About</a>
+          <a href="#">Jobs</a>
+          <a href="#">Press</a>
+          <a href="#">Blog</a>
+        </div>
+        <div class="flex flex-col space-y-3">
+          <a href="#">Contacts</a>
+          <a href="#">Terms</a>
+          <a href="#">Privacy</a>
+        </div>
+      </div>
+
+      <!-- Social -->
+      <div class="flex justify-center pb-10 space-x-3">
+        <div>
+          <a href="#">
+            <img src="/images/facebook.svg" alt="" class="p-2 bg-darkBlue rounded-full ficon">
+          </a>
+        </div>
+        <div>
+          <a href="#">
+            <img src="/images/twitter.svg" alt="" class="p-2 bg-darkBlue rounded-full ficon">
+          </a>
+        </div>
+        <div>
+          <a href="#">
+            <img src="/images/instagram.svg" alt="" class="p-2 bg-darkBlue rounded-full ficon">
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</footer>
